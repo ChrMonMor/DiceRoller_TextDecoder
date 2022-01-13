bc55d0ead9dbc4f4a5a4979529b51763bf430278
Christian Monrad Mortensen <chri615w@edu.sde.dk>
2022-01-13 15:14:31 +0100

    First stat is complet. It can only handle one group roll as of this version of the program and it can not put two groups together. nor can it add static varibles to the total of a groups, as it doesn't check for it, but you can have as many groups as you want. It doesn't validate the dice-line, so it can still throw up errors.

---
+++ DiceRoller_TextDecoder/DiceRoller_TextDecoder/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller_TextDecoder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*d*+*d*\n");
+            Console.Write("Type in you dice roll: ");
+            string diceString = Console.ReadLine();
+            DiceStringSpliter(diceString);
+            Console.ReadLine();
+
+        }
+        //-- Splits the string into smaller chucks
+        public static void DiceStringSpliter(string diceString)
+        {
+            string[] diceSets = diceString.Split('+');
+            foreach (string set in diceSets)
+            {
+                string[] diceGroup = set.Split('d');
+                DiceResultPrinter(DiceRoller(Convert.ToInt32(diceGroup[0]), Convert.ToInt32(diceGroup[1])));
+            }
+            // Code...
+        }
+        //-- Rolls the dices and returns int[]array, put total in last index
+        public static int[] DiceRoller(int diceSum, int diceSize)
+        {
+            Random random = new Random();
+            int[] result = new int[diceSum+1];
+            int total = 0;
+            for (int i = 0; i < diceSum; i++)
+            {
+                result[i] = random.Next(1, diceSize + 1);
+                total += result[i];
+            }
+            result[diceSum] = total;
+            return result;
+        }
+        //-- prints result out to console
+        public static void DiceResultPrinter(int[] diceArray)
+        {
+            foreach (var item in diceArray)
+            {
+                if (item != diceArray.Last())
+                {
+                    Console.Write(item + ", ");
+                }
+                else
+                {
+                    Console.WriteLine("Result = " +item);
+                }
+            }
+        }
+    }
+}
