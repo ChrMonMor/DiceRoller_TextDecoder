25b2ce4434c97bebf2cc91f0de477289ad9b75f9
Christian Monrad Mortensen <chri615w@edu.sde.dk>
2022-01-14 11:33:04 +0100

    Add a unit test too the project, because i got asked too do so. I do not know how to unit test a random dice roller.

---
+++ DiceRoller_TextDecoder/DiceRoller_TextDecoder_UnitTest/DiceRollerTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using DiceRoller_TextDecoder;
+
+namespace DiceRoller_TextDecoder_UnitTest
+{
+    [TestClass]
+    public class DiceRollerTest
+    {
+        [TestMethod]
+        public void Validator_True()
+        {
+            //-- Arrange
+            string testString = "5d6";
+            bool expected = true;
+
+            //-- Act
+            var actaul = Program.Validator(testString);
+
+            //-- Assert
+            Assert.AreEqual(expected, actaul);
+        }
+        [TestMethod]
+        public void Validator_False()
+        {
+            //-- Arrange
+            string testString = "5dd6";
+            bool expected = false;
+
+            //-- Act
+            var actaul = Program.Validator(testString);
+
+            //-- Assert
+            Assert.AreEqual(expected, actaul);
+        }
+    }
+}
---
+++ DiceRoller_TextDecoder/DiceRoller_TextDecoder_UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DiceRoller_TextDecoder_UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DiceRoller_TextDecoder_UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2fa83337-f416-4bc5-9d5e-82fd7b5fb545")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
